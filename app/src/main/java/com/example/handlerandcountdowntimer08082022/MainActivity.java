c5a29a2f3e116e9d294a1e73b0ed6d8ed1e1c30a
Phat <phatdroid94@gmail.com>
2022-09-30 21:11:35 +0700

    finsh

--- app/src/main/java/com/example/handlerandcountdowntimer08082022/MainActivity.java
+++ app/src/main/java/com/example/handlerandcountdowntimer08082022/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.handlerandcountdowntimer08082022;
 
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -35,30 +36,40 @@ public class MainActivity extends AppCompatActivity {
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!isRunning) {
-                    isRunning = true;
-                    countDownTimer = new CountDownTimer(1200, 1000) {
-                        @Override
-                        public void onTick(long l) {
-                            if (l >= 1000) {
-                                count = count == arrImages.length - 1 ? 0 : count;
-                                imageView.setImageResource(arrImages[count++]);
-                            }
-                        }
-
-                        @Override
-                        public void onFinish() {
-                            countDownTimer.start();
-                        }
-                    };
-
-                    countDownTimer.start();
-                } else {
-                    Toast.makeText(MainActivity.this, "Banner đang chạy", Toast.LENGTH_SHORT).show();
-                }
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "Đã chờ 1s", Toast.LENGTH_SHORT).show();
+                    }
+                }, 1000);
             }
         });
 
         // Requirement : Nếu đang chạy thì dừng không thì thông báo chưa start
+    }
+
+    private void startBanner() {
+        if (!isRunning) {
+            isRunning = true;
+            countDownTimer = new CountDownTimer(1200, 1000) {
+                @Override
+                public void onTick(long l) {
+                    if (l >= 1000) {
+                        count = count == arrImages.length - 1 ? 0 : count;
+                        imageView.setImageResource(arrImages[count++]);
+                    }
+                }
+
+                @Override
+                public void onFinish() {
+                    countDownTimer.start();
+                }
+            };
+
+            countDownTimer.start();
+        } else {
+            Toast.makeText(MainActivity.this, "Banner đang chạy", Toast.LENGTH_SHORT).show();
+        }
     }
 }
