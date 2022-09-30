a3ad1190eabe0548320b3ca050f9539434ed7130
Phat <phatdroid94@gmail.com>
2022-09-30 21:00:29 +0700

    button start

--- app/src/main/java/com/example/handlerandcountdowntimer08082022/MainActivity.java
+++ app/src/main/java/com/example/handlerandcountdowntimer08082022/MainActivity.java
@@ -1,34 +1,64 @@
 package com.example.handlerandcountdowntimer08082022;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     CountDownTimer countDownTimer;
+    ImageView imageView;
+    Button btnStart, btnPause;
+    int[] arrImages = {
+            R.drawable.image_1,
+            R.drawable.image_2,
+            R.drawable.image_3,
+            R.drawable.image_4,
+            R.drawable.image_5
+    };
+    int count = 0;
+    boolean isRunning = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Count down
-        countDownTimer = new CountDownTimer(5000, 500) {
+        imageView = findViewById(R.id.image_view_banner);
+        btnStart = findViewById(R.id.button_start);
+        btnPause = findViewById(R.id.button_pause);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onTick(long l) {
-                if (l >= 1000) {
-                    Log.d("BBB", l + "");
+            public void onClick(View view) {
+                if (!isRunning) {
+                    isRunning = true;
+                    countDownTimer = new CountDownTimer(1200, 1000) {
+                        @Override
+                        public void onTick(long l) {
+                            if (l >= 1000) {
+                                count = count == arrImages.length - 1 ? 0 : count;
+                                imageView.setImageResource(arrImages[count++]);
+                            }
+                        }
+
+                        @Override
+                        public void onFinish() {
+                            countDownTimer.start();
+                        }
+                    };
+
+                    countDownTimer.start();
+                } else {
+                    Toast.makeText(MainActivity.this, "Banner đang chạy", Toast.LENGTH_SHORT).show();
                 }
             }
+        });
 
-            @Override
-            public void onFinish() {
-                countDownTimer.start();
-                Log.d("BBB", "onFinish");
-            }
-        };
-        countDownTimer.start();
+        // Requirement : Nếu đang chạy thì dừng không thì thông báo chưa start
     }
 }
